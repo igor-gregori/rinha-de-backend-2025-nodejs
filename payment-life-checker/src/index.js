7e38f547bc64c3677b147d48f1783f475749da1c
Igor Grégori <igsilvacarlos@gmail.com>
2025-07-18 02:23:11 -0300

    feat: consume os endpoints de 'service-health' e coloca o resultado no cache

--- payment-life-checker/src/index.js
+++ payment-life-checker/src/index.js
@@ -9,26 +9,48 @@ const redisClient = new IORedis({
 let timeoutId;
 const deplayInMs = 500;
 
+let processorsStatus = {
+  default: {},
+  fallback: {},
+};
+
+// TODO: Maybe i can use setInterval and work with AbortSignal
 function recursiveLifeChecker() {
   timeoutId = setTimeout(async () => {
-    const res = await redisClient.ping();
-    console.log(res);
+    const [defaultResponse, fallbackResponse] = await Promise.all([
+      fetch(`${process.env.PAYMENT_PROCESSOR_URL_DEFAULT}/payments/service-health`),
+      fetch(`${process.env.PAYMENT_PROCESSOR_URL_FALLBACK}/payments/service-health`),
+    ]);
+    if (defaultResponse.ok) {
+      const defaultResponseBody = await defaultResponse.json();
+      processorsStatus.default = defaultResponseBody;
+    } else {
+      console.warn("Unable to verify status of default payment processor");
+    }
+    if (fallbackResponse.ok) {
+      const fallbackResponseBody = await fallbackResponse.json();
+      processorsStatus.fallback = fallbackResponseBody;
+    } else {
+      console.warn("Unable to verify status of fallback payment processor");
+    }
+    await redisClient.set("processors-status", JSON.stringify(processorsStatus));
+    console.info("Status of payment processors updated");
     recursiveLifeChecker();
   }, deplayInMs);
 }
 
 recursiveLifeChecker();
 
-console.log(`Recursive life checker running with ${deplayInMs}ms deplay is running`);
+console.info(`Recursive life checker running with ${deplayInMs}ms deplay is running`);
 
 process.on("SIGTERM", async () => {
-  console.log("SIGTERM received. Stopping recursive life checker...");
+  console.info("SIGTERM received. Stopping recursive life checker...");
   clearTimeout(timeoutId);
   process.exit(0);
 });
 
 process.on("SIGINT", async () => {
-  console.log("SIGTERM received. Stopping recursive life checker...");
+  console.info("SIGTERM received. Stopping recursive life checker...");
   clearTimeout(timeoutId);
   process.exit(0);
 });
