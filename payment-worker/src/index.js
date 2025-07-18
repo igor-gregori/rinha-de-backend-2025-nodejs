90b7fa1921dab803625b09a05be7cdda134463c3
Igor Grégori <igsilvacarlos@gmail.com>
2025-07-18 02:25:28 -0300

    feat: process payment

--- payment-worker/src/index.js
+++ payment-worker/src/index.js
@@ -1,20 +1,35 @@
 import { Worker } from "bullmq";
 import IORedis from "ioredis";
 
-import { setTimeout } from "timers/promises";
-
 const redisClient = new IORedis({
   host: "redis",
   port: 6379,
   maxRetriesPerRequest: null,
 });
 
+// TODO: Find out how i handle with errors
+// TODO: Find out how i handle with concurrency
 const worker = new Worker(
   "payment",
   async (job) => {
-    console.log("processando pagamento...");
-    await setTimeout(100);
-    console.log(job.data);
+    try {
+      const payment = JSON.parse(job.data);
+
+      const processorsStatusResponse = await redisClient.get("processors-status");
+      const processorsStatus = JSON.parse(processorsStatusResponse);
+
+      if (processorsStatus.default.failing === false) {
+        await sendPayment(process.env.PAYMENT_PROCESSOR_URL_DEFAULT, payment);
+        console.info("🟢 Payment processed with default processor");
+      } else if (processorsStatus.fallback.failing === false) {
+        await sendPayment(process.env.PAYMENT_PROCESSOR_URL_FALLBACK, payment);
+        console.info("🟠 Payment processed with fallback processor");
+      } else {
+        console.warn("🔴 Payment not processed");
+      }
+    } catch (err) {
+      console.error(err);
+    }
   },
   {
     connection: redisClient,
@@ -22,9 +37,19 @@ const worker = new Worker(
   }
 );
 
-worker.on("ready", () => {
-  console.log("Worker ready");
-});
+console.log("Worker started");
+
+async function sendPayment(url, payment) {
+  return fetch(`${url}/payments`, {
+    method: "POST",
+    body: JSON.stringify({
+      correlationId: payment.correlationId,
+      amount: payment.amount,
+      requestedAt: new Date().toISOString(),
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+}
 
 process.on("SIGTERM", async () => {
   console.log("SIGTERM received. Closing worker...");
